5b0dde993880777c660d7ce6e9c55863ad8da0b7
Ian Mota <ianmotabr@gmail.com>
2022-06-26 11:53:45 -0300

    services-verificacoes

--- adapters/colmoBambu.py
+++ adapters/colmoBambu.py
@@ -1,5 +1,6 @@
 from statistics import median
 from entites.propriedadesGeometricas import Geometria
+from services.verificacoesNormativas import BarraPrismatica
 
 class colmoDeBambu():
     def __init__(self,geometria1:Geometria,geometria2:Geometria,comprimento:float) -> None:
@@ -20,23 +21,14 @@ class colmoDeBambu():
             float: diâmetro do colmo
         """
         
-        D = median([self.e1.diametroMedio(),self.e2.diametroMedio()])
-        
-        if(65*D <= self.L):
-            return(D)
-        
-        else:
-            return("A barra não pode ser tratada como prismática, 65*D > L")
-        
+        self.D = median([self.e1.diametroMedio(),self.e2.diametroMedio()])
+        return(BarraPrismatica(self.D,self.L,self.D))
+
     def Espessura(self) -> float:
-        
         t = median([self.e1.espessuraMedia(),self.e2.espessuraMedia()])
-        if(65*self.DiametroExterno() <= self.L):
-            return(t)
-        
-        else:
-            return("A barra não pode ser tratada como prismática, 65*D > L")
+        return(BarraPrismatica(self.D,self.L,t))
         
     def DiametroInterno(self) -> float:
         d = self.DiametroExterno() - 2*self.Espessura()
+        return(BarraPrismatica(self.D,self.L,d))
         
--- services/verificacoesNormativas.py
+++ services/verificacoesNormativas.py
@@ -1,6 +1,17 @@
-def BarraPrismatica(D,L):
+def BarraPrismatica(D:float,L:float,parametro)->bool:
+        """Verifica se o colmo com as dimensões D e L pode ser considerado como prismático
+
+        Args:
+        D (float): diâmetro
+        L (float): comprimento
+
+        Returns:
+        bool: pode ser / não pode ser
+        """
         if(65*D <= L):
-            return(t)
+            return(parametro)
         
         else:
             return("A barra não pode ser tratada como prismática, 65*D > L")
+        
+        
